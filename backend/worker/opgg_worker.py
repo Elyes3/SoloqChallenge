c4fa73f10e121d7187e8e6251e6590435859c3bc
Mohamed Amine <amine.elamri@gmail.com>
2023-05-26 13:30:52 +0100

    updated op.gg worker

--- backend/worker/opgg_worker.py
+++ backend/worker/opgg_worker.py
@@ -3,69 +3,55 @@ from requests_html import HTMLSession
 from bs4 import *
 from tabulate import tabulate
 import sys
-
+import json
 
 def run(arg):
-	server = "euw"
 	player = arg
-
-	################################################################################
-	ranks = ["challenger","grandmaster","master","diamond 1","diamond 2","diamond 3","diamond 4","platinum 1","platinum 2","platinum 3","platinum 4","gold 1","gold 2","gold 3","gold 4","silver 1","silver 2","silver 3","silver 4","bronze 1","bronze 2","bronze 3","bronze 4","iron 1","iron 2","iron 3", "iron 4","unranked"]
-
 
 	session = HTMLSession()
 
 
-	page = 'https://' + server + '.op.gg/summoner/userName=' + player.replace(" ","+")
+	page = f'https://euw.op.gg/summoner/userName={player}'
 	html = session.get(page)
 	soup = BeautifulSoup(html.content, 'html.parser')
 
+	
 	output = {}
-	try:
-		rank = soup.find("div", {"class": "tier"}).text
-		LP = soup.find("div", {"class": "lp"})
-	except Exception:
-		print("Summoner not found")
-		output['summoner'] = player
-		output['status'] = "NOT_FOUND"
-		return output
-
-	if LP is not None:
-		LP = LP.text
-	else:
-		LP = "0 LP"
-
-	win_lose = soup.find("div", {"class": "win-lose"})
-	if win_lose is not None:
-		wins = int(win_lose.text[:win_lose.text.index("W")])
-		losses = int(win_lose.text[win_lose.text.index("W")+2:win_lose.text.index("L")])
-	else:
-		wins = 0
-		losses = 0
-
-	winrate = 0
-	winratio = soup.find("div", {"class": "ratio"})
-	if winratio is not None:
-		temp = winratio.text.replace("Win Rate ", "")
-		temp = temp.replace("%", "")
-		winrate = int(temp)
-
-	if LP is not None:
-		league = rank
-	else:
-		league = "Unranked"
-
-	if wins is not None:
-		games = wins+losses
-
+	output['status'] = 'NOT_FOUND'
 	output['summoner'] = player
-	output['tier'] = league
-	output['wins'] = wins
-	output['losses'] = losses
-	output['games'] = games
-	output['lp'] = LP
-	output['winrate'] = winrate
-	output['status'] = "OK"
+	try:
+		data_raw = soup.find("script", {"id": "__NEXT_DATA__"}).text
+		json_raw = json.loads(data_raw)
+	
+		data = json_raw['props']['pageProps']['data']
+		
+		output = {
+		'summoner':data['name'],
+		'icon_url':data['profile_image_url'],
+		'level':data['level'],
+		'summoner_id':data['summoner_id'],
+		}
+		
+		for queue in data['league_stats']:
+			if queue['queue_info']['game_type'] == 'SOLORANKED':
+				output['tier'] = queue['tier_info']['tier']
+				output['division'] = queue['tier_info']['division']
+				output['lp'] = queue['tier_info']['lp']
+				output['tier_image_url'] = queue['tier_info']['tier_image_url']
+				output['border_image_url'] = queue['tier_info']['border_image_url']
+				output['win'] = queue['win']
+				output['lose'] = queue['lose']
+				output['games'] = output['win'] + output['lose']
+				output['is_hot_streak'] = queue['is_hot_streak']
+				output['series'] = queue['series']
+				output['last_updated_at'] = queue['updated_at']
+				output['status'] = 'OK'
+	except Exception as e:
+		print(f"{e}: Cannot be found! url='{page}'")
+		output['status'] = 'NOT_FOUND'
+		
+	print(output)
+
 	return output
 
 
