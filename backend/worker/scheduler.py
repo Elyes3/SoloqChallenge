6b89db9f91c5c8c5e2f571a66928f7ce7d5db145
Amine Amri <amine.elamri@gmail.com>
2023-05-28 15:43:00 +0100

    debug messages for server logs

--- backend/worker/scheduler.py
+++ backend/worker/scheduler.py
@@ -8,7 +8,8 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file\
 api_url = str(os.getenv('API_URL'))
-summoner_update_interval = str(os.getenv('UPDATE_SUMMONERS_EVERY'))
+summoner_update_interval = int(os.getenv('UPDATE_SUMMONERS_EVERY'))
+summoner_renew_wait = int(os.getenv('SUMMONER_RENEW_WAIT'))
 
 
 def fetch_summoners():
@@ -41,24 +42,31 @@ def op_gg_renew(summoner_id):
 
 
 def update_all_summoners():
+    print("Summoner profile update started")
     # fetch all summoners
     players = fetch_summoners()
+    print(f"{len(players)} profiles fetched")
     # renew all players data
     for player in players:
         # renew the op.gg data
         op_gg_renew(player['summoner_id'])
+    print(f"called  op.gg renew, grace period set to {summoner_renew_wait} minutes")
 
     # wait for op.gg servers to renew the data
-    time.sleep(5)
+    time.sleep(summoner_renew_wait)
+
+    print("grace period ended, fetching op.gg profile data")
 
     for player in players:
         data = opgg_worker.run(player['summoner'])
         # update the data in the db
         update_summoner(player['id'], data)
 
+    print("Job ended successfully!")
+
 
 schedule.every(summoner_update_interval).minutes.do(update_all_summoners)
-
+print(f"Update summoners scheduled to run every {summoner_update_interval} minutes")
 # Loop so that the scheduling task
 # keeps on running all time.
 while True:
